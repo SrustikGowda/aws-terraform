8a2a0924b6f910ed409b509e153e1d29136c777c
Srustik D <dsr@omnissa.com>
2025-12-16 22:56:01 +0530

    Enable S3 backend for CI/CD destroy to work properly

--- provider.tf
+++ provider.tf
@@ -8,15 +8,15 @@ terraform {
     }
   }
 
-  # Using local backend for simplicity (for college projects)
-  # Uncomment the S3 backend below if you want to use remote state
-  # backend "s3" {
-  #   bucket         = "terraform-state-bucket"
-  #   key            = "ec2/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-locks"
-  #   encrypt        = true
-  # }
+  # S3 backend for remote state (required for CI/CD destroy to work)
+  # For local development, you can comment this out and use local state
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "ec2/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
